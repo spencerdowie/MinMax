fa3c08f9e6b951c60e5cef354db5b7e4f9280d43
spencer_dowie <spencer.dowie@uoit.net>
2020-03-03 09:52:49 -0500

    [+] Initial

---
+++ Assets/Scenes/ClickHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ClickHandler : MonoBehaviour, IPointerDownHandler
+{
+    int index;
+    GameManager manager;
+
+    private void Start()
+    {
+        index = transform.GetSiblingIndex();
+        manager = FindObjectOfType<GameManager>();
+    }
+
+    public void OnPointerDown(PointerEventData e)
+    {
+        
+    }
+}
---
+++ Assets/Scenes/GameManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerType
+{
+    X = 1,
+    O = -1
+}
+public class GameManager : MonoBehaviour
+{
+    public int[] tiles = new int[9];
+    public PlayerType player;
+
+    public void ClickTile(int i)
+    {
+        if(tiles[i] == 0)
+        {
+            tiles[i] = (int)player;
+        }
+    }
+
+    public int MinMax()
+    {
+
+    }
+
+    public int CheckWin()
+    {
+        int win = 0;
+        for(int i = 0; i < 9; i+=3)
+        {
+            win = tiles[i] + tiles[i + 1] + tiles[i + 2];
+            if (win == 3 || win == -3)
+                return win;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            win = tiles[i] + tiles[i + 3] + tiles[i + 6];
+            if (win == 3 || win == -3)
+                return win;
+        }
+        win = tiles[0] + tiles[4] + tiles[8];
+        if (win == 3 || win == -3)
+            return win;
+        win = tiles[2] + tiles[4] + tiles[6];
+        if (win == 3 || win == -3)
+            return win;
+        return 0;
+    }
+}
